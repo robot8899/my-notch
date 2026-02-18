0e7d91b14f127b3d1678f0743633af44522888e2
robot8899 <2580796679@qq.com>
2026-02-19 04:14:44 +0800

    Initial Commit

---
+++ notch/notchApp.swift
@@ -0,0 +1,17 @@
+//
+//  notchApp.swift
+//  notch
+//
+//  Created by 罗彬 on 2026/2/19.
+//
+
+import SwiftUI
+
+@main
+struct notchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ notchTests/notchTests.swift
@@ -0,0 +1,17 @@
+//
+//  notchTests.swift
+//  notchTests
+//
+//  Created by 罗彬 on 2026/2/19.
+//
+
+import Testing
+@testable import notch
+
+struct notchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
